c8d624647b0be41d8f643319abb253bda3464861
Martin Giessing <mgp@novicell.dk>
2021-03-09 19:42:14 +0100

    Add to root package attempt

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -2,8 +2,14 @@
 namespace Premium\composer;
 
 use Composer\Command\RequireCommand;
+use Composer\Package\Link;
+use Composer\Plugin\PluginInterface;
 use Composer\Script\Event;
+use Composer\Semver\Constraint\Constraint;
+use Composer\Semver\Constraint\MatchAllConstraint;
+use JsonSchema\Constraints\UndefinedConstraint;
 use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
 
 class ScriptHandler {
   private static $optional_modules = [
@@ -28,27 +34,36 @@ class ScriptHandler {
    */
   public static function postRootPackageInstall(Event $event) {
     $information = [];
-    if (!empty($domain_name = $event->getIO()->ask('Domain name:'))) {
-      $information['domain_name'] = $domain_name;
+    /*if (!empty($project_name = $event->getIO()->ask('Project name:'))) {
+        $information['project_name'] = $project_name;
     }
-    if (!empty($project_name = $event->getIO()->ask('Project name:'))) {
-      $information['project_name'] = $project_name;
+    if (!empty($domain_name = $event->getIO()->ask('Domain name:'))) {
+        $information['domain_name'] = $domain_name;
     }
     if ($values = $event->getIO()->select('Optional modules', array_keys(self::$optional_modules), 'none', FALSE, 'Value "%s" is invalid', TRUE)) {
-      if (is_array($values)) {
-        $information['optional_modules'] = $values;
-      }
-    }
+        if (is_array($values)) {
+            $information['optional_modules'] = $values;
+        }
+    }*/
+    $project_name = 'test';
+    $domain_name = 'test.dk';
+    $values = [0];
 
+    $links = $event->getComposer()->getPackage()->getRequires();
+    $links[] = new Link('novicell/drupal-premium', 'drupal/cookiebot', new Constraint('>=', '2.0'));
+    $event->getComposer()->getPackage()->setRequires($links);
     foreach ($values as $choice) {
       $packages = array_values(self::$optional_modules)[$choice];
-      $command = new RequireCommand();
+      /*$command = new RequireCommand();
       $input = new ArrayInput(array(
-        'command' => 'require',
-        $command->getDefinition()
-      ));
-      $input->setArgument('packages', $packages);
-      $command->run($input);
+          'packages' => $packages,
+          ),
+          $command->getDefinition()
+      );
+      $command->addOption('no-plugins');
+      $command->setComposer($event->getComposer()->);
+      $command->setApplication();
+      $command->run($input, new NullOutput());*/
     }
 
     $site_dir = 'webroot/sites/' . $domain_name;
