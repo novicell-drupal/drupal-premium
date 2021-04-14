ca91eb637d12d210c9f950c395c187f515e2d6b9
Kasper Basse <kdb@novicell.dk>
2021-04-14 16:13:21 +0200

    Add hero, but missing video.

--- webroot/modules/custom/premium_theme_helper/src/Plugin/Block/PageHeaderBlock.php
+++ webroot/modules/custom/premium_theme_helper/src/Plugin/Block/PageHeaderBlock.php
@@ -1,9 +1,14 @@
 <?php
+
 namespace Drupal\premium_theme_helper\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Logger\LoggerChannelInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\TypedData\Exception\MissingDataException;
 use Drupal\paragraphs\Entity\Paragraph;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Routing\Route;
@@ -17,33 +22,57 @@ use Symfony\Component\Routing\Route;
  *   category= @Translation("Premium")
  * )
  */
-class PageHeaderBlock extends BlockBase implements ContainerFactoryPluginInterface
-{
+class PageHeaderBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
   /**
+   * Route match.
+   *
    * @var RouteMatchInterface
    */
-  private $route_match;
+  protected $route_match;
 
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, RouteMatchInterface $route_match) {
+  /**
+   * Logger.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
+   */
+  protected $logger;
+
+  /**
+   * PageHeaderBlock constructor.
+   *
+   * @param array $configuration
+   *   The plugin configuration.
+   * @param string $plugin_id
+   *   The plugin ID.
+   * @param mixed $plugin_definition
+   *   The plugin definition.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   Route match.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger
+   *   Logger.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, RouteMatchInterface $route_match, LoggerChannelFactoryInterface $logger) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->route_match = $route_match;
+    $this->logger = $logger->get('premium_theme_helper');
   }
 
   /**
    * @inheritDoc
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    /** @var \Drupal\Core\Routing\RouteMatch $route_match */
     $route_match = $container->get('current_route_match');
-
-    return new static($configuration, $plugin_id, $plugin_definition, $route_match);
+    /** @var LoggerChannelFactoryInterface $logger */
+    $logger = $container->get('logger.factory');
+    return new static($configuration, $plugin_id, $plugin_definition, $route_match, $logger);
   }
 
   /**
    * @inheritDoc
    */
-  public function build()
-  {
+  public function build(): array {
     $build = [
       '#cache' => [
         'contexts' => ['url'],
@@ -53,14 +82,25 @@ class PageHeaderBlock extends BlockBase implements ContainerFactoryPluginInterfa
 
     /** @var \Drupal\Core\Entity\ContentEntityInterface $route_entity */
     $route_entity = $this->getEntityFromRouteMatch($this->route_match);
-    if(!empty($route_entity) && $route_entity->hasField('field_hero')) {
+    if (!is_null($route_entity) && $route_entity->hasField('field_hero')) {
       $build['#cache']['tags'] += $route_entity->getCacheTagsToInvalidate();
 
       if (!$route_entity->get('field_hero')->isEmpty()) {
-        $pid = $route_entity->get('field_hero')->first()->getValue()['target_id'];
-        $paragraph = Paragraph::load($pid);
-        $build['hero'] = \Drupal::entityTypeManager()->getViewBuilder('paragraph')->view($paragraph);
-        $build['#cache']['tags'] += $paragraph->getCacheTagsToInvalidate();
+        try {
+          $pid = $route_entity->get('field_hero')->first();
+          if (!is_null($pid)) {
+            $pid = $pid->getValue()['target_id'];
+            $paragraph = Paragraph::load($pid);
+            if (!is_null($paragraph)) {
+              $build['hero'] = \Drupal::entityTypeManager()->getViewBuilder('paragraph')->view($paragraph);
+              $build['hero']['#title'] = $route_entity->label();
+              $build['#cache']['tags'] += $paragraph->getCacheTagsToInvalidate();
+            }
+          }
+        }
+        catch (MissingDataException $e) {
+          $this->logger->error($e->getMessage());
+        }
       }
     }
 
@@ -76,7 +116,7 @@ class PageHeaderBlock extends BlockBase implements ContainerFactoryPluginInterfa
    * @return \Drupal\Core\Entity\EntityInterface|null
    *   The entity, or null if it's not an entity route.
    */
-  protected function getEntityFromRouteMatch(RouteMatchInterface $route_match) {
+  protected function getEntityFromRouteMatch(RouteMatchInterface $route_match): ?EntityInterface {
     $route = $route_match->getRouteObject();
     if (!$route) {
       return NULL;
@@ -99,7 +139,7 @@ class PageHeaderBlock extends BlockBase implements ContainerFactoryPluginInterfa
    * @return string|null
    *   The entity type id, null if it doesn't exist.
    */
-  protected function getEntityTypeFromRoute(Route $route) {
+  protected function getEntityTypeFromRoute(Route $route): ?string {
     if (!empty($route->getOptions()['parameters'])) {
       foreach ($route->getOptions()['parameters'] as $option) {
         if (isset($option['type']) && strpos($option['type'], 'entity:') === 0) {
